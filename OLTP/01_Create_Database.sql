388bf62ed30a00a44f7f3032b637e96fe9b47478
Koteswararao-Ponnada <koteswararao.ponnada18@gmail.com>
2026-07-23 02:50:02 +0530

    Updated OLTP database creation script

--- OLTP/01_Create_Database.sql
+++ OLTP/01_Create_Database.sql
@@ -1,13 +1,31 @@
+/*==============================================================
+ Project        : Enterprise Retail Data Warehouse
+ Module         : OLTP Database
+ Script Name    : 01_Create_Database.sql
+ Author         : Koteswararao Ponnada
+ Description    : Creates the Enterprise Retail OLTP database
+==============================================================*/
+
 USE master;
 GO
 
+-- Drop database if it already exists
 IF DB_ID('EnterpriseRetailOLTP') IS NOT NULL
-DROP DATABASE EnterpriseRetailOLTP;
+BEGIN
+    ALTER DATABASE EnterpriseRetailOLTP
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+    DROP DATABASE EnterpriseRetailOLTP;
+END;
 GO
 
+-- Create Database
 CREATE DATABASE EnterpriseRetailOLTP;
 GO
 
+-- Use Database
 USE EnterpriseRetailOLTP;
 GO
 
+PRINT 'EnterpriseRetailOLTP database created successfully.';
+GO
