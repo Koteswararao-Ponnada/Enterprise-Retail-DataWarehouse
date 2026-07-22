682468aa4f3cc9bb8bf1401a8eb12fe8b00ebe8f
Koteswararao-Ponnada <koteswararao.ponnada18@gmail.com>
2026-07-23 02:39:11 +0530

    Implemented initial OLTP database design

---
+++ OLTP/01_Create_Database.sql
@@ -0,0 +1,13 @@
+USE master;
+GO
+
+IF DB_ID('EnterpriseRetailOLTP') IS NOT NULL
+DROP DATABASE EnterpriseRetailOLTP;
+GO
+
+CREATE DATABASE EnterpriseRetailOLTP;
+GO
+
+USE EnterpriseRetailOLTP;
+GO
+
