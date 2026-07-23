ed3e06f4f37fd1ef1b7aad190670c46d657bef37
Koteswararao-Ponnada <koteswararao.ponnada18@gmail.com>
2026-07-24 02:33:43 +0530

    Generated Reference data in new folder

---
+++ OLTP/Utilities/Data Generation/01_Generate_ReferenceData.sql
@@ -0,0 +1,67 @@
+/*==============================================================
+Part A : Reference Data
+==============================================================*/
+
+USE EnterpriseRetailOLTP;
+GO
+
+INSERT INTO Reference.OrderStatus
+(StatusName, StatusDescription)
+VALUES
+('Pending','Order has been placed'),
+('Processing','Order is being processed'),
+('Packed','Order has been packed'),
+('Shipped','Order has been shipped'),
+('Delivered','Order delivered successfully'),
+('Cancelled','Order cancelled'),
+('Returned','Customer returned the order');
+GO
+
+INSERT INTO Reference.PaymentMethods
+(PaymentMethodName)
+VALUES
+('Cash'),
+('Credit Card'),
+('Debit Card'),
+('UPI'),
+('Net Banking'),
+('Gift Card');
+GO
+
+INSERT INTO Reference.PaymentStatus
+(StatusName)
+VALUES
+('Pending'),
+('Success'),
+('Failed'),
+('Refunded');
+GO
+
+INSERT INTO Reference.ShipmentStatus
+(StatusName)
+VALUES
+('Pending'),
+('Packed'),
+('In Transit'),
+('Delivered'),
+('Returned');
+GO
+
+INSERT INTO Reference.ReturnReasons
+(ReasonName)
+VALUES
+('Damaged Product'),
+('Wrong Product'),
+('Quality Issue'),
+('Late Delivery'),
+('Customer Changed Mind');
+GO
+
+INSERT INTO Reference.ReturnStatus
+(StatusName)
+VALUES
+('Requested'),
+('Approved'),
+('Rejected'),
+('Refunded');
+GO
