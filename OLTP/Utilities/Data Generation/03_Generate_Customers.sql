9c54b6b69dc42b42ac6a86f36e01a42ed220bec3
Koteswararao-Ponnada <koteswararao.ponnada18@gmail.com>
2026-07-24 03:01:25 +0530

    Generated customers data

--- OLTP/Utilities/Data Generation/03_Generate_Customers.sql
+++ OLTP/Utilities/Data Generation/03_Generate_Customers.sql
@@ -6,36 +6,104 @@ Project        : Enterprise Retail Data Warehouse
 Module         : Data Generation
 Script Name    : 03_Generate_Customers.sql
 Author         : Koteswararao Ponnada
-Description    : Generates 10,000 realistic customers
+Description    : Generate 10,000 realistic customers
 ==============================================================*/
 
 SET NOCOUNT ON;
 GO
 
 /*==============================================================
-Delete Existing Data
+Delete Existing Customers
 ==============================================================*/
 
-TRUNCATE TABLE Master.Customers;
+DELETE FROM Master.Customers;
 GO
 
 /*==============================================================
-Variable Initialization
+Load Lookup Data
 ==============================================================*/
 
-DECLARE @FirstNameCount INT;
-DECLARE @LastNameCount INT;
-DECLARE @EmailDomainCount INT;
-DECLARE @CityCount INT;
+IF OBJECT_ID('tempdb..#FirstNames') IS NOT NULL DROP TABLE #FirstNames;
+IF OBJECT_ID('tempdb..#LastNames') IS NOT NULL DROP TABLE #LastNames;
+IF OBJECT_ID('tempdb..#EmailDomains') IS NOT NULL DROP TABLE #EmailDomains;
 
-SELECT @FirstNameCount = COUNT(*) FROM Utility.FirstNames;
-SELECT @LastNameCount = COUNT(*) FROM Utility.LastNames;
-SELECT @EmailDomainCount = COUNT(*) FROM Utility.EmailDomains;
-SELECT @CityCount = COUNT(*) FROM Master.Cities;
+SELECT
+    ROW_NUMBER() OVER(ORDER BY FirstNameID) AS RN,
+    FirstName
+INTO #FirstNames
+FROM Utility.FirstNames;
+
+SELECT
+    ROW_NUMBER() OVER(ORDER BY LastNameID) AS RN,
+    LastName
+INTO #LastNames
+FROM Utility.LastNames;
+
+SELECT
+    ROW_NUMBER() OVER(ORDER BY DomainID) AS RN,
+    DomainName
+INTO #EmailDomains
+FROM Utility.EmailDomains;
+GO
+
+DECLARE @FirstNameCount INT =
+(
+    SELECT COUNT(*) FROM #FirstNames
+);
+
+DECLARE @LastNameCount INT =
+(
+    SELECT COUNT(*) FROM #LastNames
+);
+
+DECLARE @EmailDomainCount INT =
+(
+    SELECT COUNT(*) FROM #EmailDomains
+);
+
+DECLARE @CityCount INT =
+(
+    SELECT COUNT(*) FROM Master.Cities
+);
 
 /*==============================================================
 Generate Customers
 ==============================================================*/
+
+;WITH CustomerData AS
+(
+    SELECT TOP (10000)
+
+        N.Number,
+
+        FN.FirstName,
+
+        LN.LastName,
+
+        ED.DomainName,
+
+        CityID =
+            ((N.Number - 1) % @CityCount) + 1,
+
+        Gender =
+            CASE
+                WHEN N.Number % 2 = 0 THEN 'M'
+                ELSE 'F'
+            END
+
+    FROM Utility.Numbers N
+
+    INNER JOIN #FirstNames FN
+        ON FN.RN = ((N.Number - 1) % @FirstNameCount) + 1
+
+    INNER JOIN #LastNames LN
+        ON LN.RN = (((N.Number * 7) - 1) % @LastNameCount) + 1
+
+    INNER JOIN #EmailDomains ED
+        ON ED.RN = (((N.Number * 3) - 1) % @EmailDomainCount) + 1
+
+    ORDER BY N.Number
+)
 
 INSERT INTO Master.Customers
 (
@@ -46,98 +114,95 @@ INSERT INTO Master.Customers
     , Email
     , PhoneNumber
     , CityID
+    , AddressLine
+    , PostalCode
+    , CustomerType
     , RegistrationDate
     , CreatedDate
     , ModifiedDate
     , IsActive
 )
 
-SELECT TOP (10000)
+SELECT
 
-      FN.FirstName
+      FirstName
 
-    , LN.LastName
+    , LastName
 
-    , CASE
-            WHEN N.Number % 2 = 0 THEN 'M'
-            ELSE 'F'
-      END
+    , Gender
 
     , DATEADD
       (
             DAY,
-            -(18*365 + ABS(CHECKSUM(NEWID())) % (52*365)),
+            -((18 * 365) + (ABS(CHECKSUM(NEWID())) % (52 * 365))),
             CAST(GETDATE() AS DATE)
       )
 
     , LOWER
       (
-            FN.FirstName
+            FirstName
             + '.'
-            + LN.LastName
-            + CAST(N.Number AS VARCHAR(10))
+            + LastName
+            + CAST(Number AS VARCHAR(10))
             + '@'
-            + ED.DomainName
+            + DomainName
       )
 
     , CONCAT
       (
             '9',
-            RIGHT('000000000' + CAST(ABS(CHECKSUM(NEWID())) % 1000000000 AS VARCHAR(9)),9)
+            RIGHT('000000000' +
+            CAST(Number AS VARCHAR(9)),9)
       )
 
-    , ((N.Number-1) % @CityCount) + 1
+    , CityID
+
+    , CONCAT
+      (
+            'House ',
+            Number,
+            ', Street ',
+            ((Number % 150) + 1)
+      )
+
+    , RIGHT
+      (
+            '000000' +
+            CAST(500000 + (Number % 99999) AS VARCHAR(6)),
+            6
+      )
+
+    , CASE
+            WHEN Number % 20 = 0
+                THEN 'Premium'
+            ELSE 'Regular'
+      END
 
     , DATEADD
       (
             DAY,
-            -(ABS(CHECKSUM(NEWID())) % 1825),
+            -(Number % 1825),
             CAST(GETDATE() AS DATE)
       )
 
-    , DATEADD
-      (
-            DAY,
-            -(ABS(CHECKSUM(NEWID())) % 1825),
-            GETDATE()
-      )
+    , GETDATE()
 
     , NULL
 
     , CASE
-            WHEN ABS(CHECKSUM(NEWID())) % 100 < 95
-            THEN 1
+            WHEN Number % 100 < 95
+                THEN 1
             ELSE 0
       END
 
-FROM Utility.Numbers N
-
-INNER JOIN Utility.FirstNames FN
-ON FN.FirstNameID =
-(
-((N.Number-1) % @FirstNameCount)+1
-)
-
-INNER JOIN Utility.LastNames LN
-ON LN.LastNameID =
-(
-((N.Number*7-1) % @LastNameCount)+1
-)
-
-INNER JOIN Utility.EmailDomains ED
-ON ED.EmailDomainID =
-(
-((N.Number*5-1) % @EmailDomainCount)+1
-)
-
-ORDER BY N.Number;
+FROM CustomerData;
 GO
 
 /*==============================================================
 Verification
 ==============================================================*/
 
-PRINT 'Customer Generation Completed';
+PRINT 'Customers Generated Successfully';
 PRINT '--------------------------------';
 
 SELECT COUNT(*) AS TotalCustomers
@@ -147,6 +212,11 @@ SELECT Gender,
        COUNT(*) AS TotalCustomers
 FROM Master.Customers
 GROUP BY Gender;
+
+SELECT CustomerType,
+       COUNT(*) AS TotalCustomers
+FROM Master.Customers
+GROUP BY CustomerType;
 
 SELECT IsActive,
        COUNT(*) AS TotalCustomers
@@ -158,11 +228,11 @@ SELECT TOP (20)
        FirstName,
        LastName,
        Gender,
-       DateOfBirth,
        Email,
        PhoneNumber,
-       CityID,
-       RegistrationDate
+       AddressLine,
+       PostalCode,
+       CustomerType
 FROM Master.Customers
 ORDER BY CustomerID;
 
@@ -174,4 +244,9 @@ FROM
     GROUP BY Email
     HAVING COUNT(*) > 1
 ) D;
+GO
+
+DROP TABLE #FirstNames;
+DROP TABLE #LastNames;
+DROP TABLE #EmailDomains;
 GO
